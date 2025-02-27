8f82cae0c7c5f28219231f11d9f45e5d7bb5d9c0
Simão Durão <simaodurao@gmail.com>
2025-02-27 11:30:11 +0000

    Added Sa and V

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,9 +7,20 @@ namespace Cilindro
         private static void Main(string[] args)
         {
             Console.WriteLine("Insere a Altura do Cilindro: ");
-            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
             Console.WriteLine("Insere o Raio do Cilindro: ");
             string c = Console.ReadLine();
+
+            double a = double.Parse(b);
+            double r = double.Parse(c);
+
+            double V = Math.PI * Math.Pow(r,2)*a;
+            
+            double Sa= 2*Math.PI*r*(r+a);
+
+            Console.WriteLine($"{V:f3}");
+            Console.WriteLine($"{Sa:f3}");
         }
     }
 }
