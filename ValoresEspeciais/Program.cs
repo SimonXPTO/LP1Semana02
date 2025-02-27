626fb299f309672ebf79bdab15eb4fe3e754edb7
Simão Durão <simaodurao@gmail.com>
2025-02-27 12:10:33 +0000

    Added Overflow and UnderFlow

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,6 +6,8 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
+            //Max Value All types
+
             Console.WriteLine(sbyte.MaxValue);
             Console.WriteLine(byte.MaxValue);
             Console.WriteLine(uint.MaxValue);
@@ -16,6 +18,8 @@ namespace ValoresEspeciais
             Console.WriteLine(Int128.MaxValue);
             Console.WriteLine(short.MaxValue);
             Console.WriteLine(decimal.MaxValue);
+
+            //Min Value All types
 
             Console.WriteLine(sbyte.MinValue);
             Console.WriteLine(byte.MinValue);
@@ -28,14 +32,35 @@ namespace ValoresEspeciais
             Console.WriteLine(short.MinValue);
             Console.WriteLine(decimal.MinValue);
 
+            //Special Value Double
+            
             Console.WriteLine(double.PositiveInfinity);
             Console.WriteLine(double.NegativeInfinity);
             Console.WriteLine(double.NaN);
 
+            //Special Value Float
+            
             Console.WriteLine(float.PositiveInfinity);
             Console.WriteLine(float.NegativeInfinity);
             Console.WriteLine(float.NaN);
 
+            //Overflow Uint
+
+            uint a = uint.MaxValue;
+            Console.WriteLine(a+1);
+
+            //Overflow Float
+
+            float b = float.MaxValue;
+            Console.WriteLine(b+1);
+            Console.WriteLine(b*2);
+
+            //Underflow Float
+
+            float f, f1;
+            f = f1 = 20000.0f;
+            Console.WriteLine(f==f1 + 0.00001f);
+            
         }
     }
 }
