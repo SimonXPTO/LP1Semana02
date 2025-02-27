dafc3b0314ee7d022c42d2464963613be20d6dc7
Simão Durão <simaodurao@gmail.com>
2025-02-27 23:33:31 +0000

    Added questions in ChatGPTOne

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            while(true)
+            {
+                string question,response;
+                Console.Write("Faz-me uma pergunta: ");
+                question = Console.ReadLine();
+
+                switch(question)
+                {
+                    case "Tens quantos anos?":
+                        response = "Acabei de nascer!";
+                        break;
+                    case "Gostas de LP1?":
+                        response = "Ainda não decidi sobre esse assunto";
+                        break;
+                    case "Que horas são?":
+                        response = "Olha para o relogio!!!";
+                        break;
+                    case "Tens fome?":
+                        response = "Tenho sempre fome";
+                        break;
+                    case "EXIT":
+                        return;
+                    default:
+                        response = "Não reconheço esta pergunta";
+                        break;
+                }
+
+                Console.WriteLine(response);
+                }
+            }
+        }
+    }
+
