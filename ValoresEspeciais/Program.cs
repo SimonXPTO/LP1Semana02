82118d36dc36f69eb393f2791df622c4f1616a26
Simão Durão <simaodurao@gmail.com>
2025-02-27 11:46:37 +0000

    Added All types Min Values I know

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -16,6 +16,17 @@ namespace ValoresEspeciais
             Console.WriteLine(Int128.MaxValue);
             Console.WriteLine(short.MaxValue);
             Console.WriteLine(decimal.MaxValue);
+            
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(ulong.MinValue);
+            Console.WriteLine(Int128.MinValue);
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(decimal.MinValue);
         }
     }
 }
