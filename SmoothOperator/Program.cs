449809c61510f75f7f07ddc0adf6ad7056a5f5c1
Simão Durão <simaodurao@gmail.com>
2025-02-27 10:56:51 +0000

    Added SmoothOperator and Increment and decrement of n

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+                Console.WriteLine("Insere número inteiro: ");
+                string a = Console.ReadLine();
+                sbyte n = sbyte.Parse(a);
+                Console.WriteLine(--n);
+                Console.WriteLine(++n);
+        }
+    }
+}
