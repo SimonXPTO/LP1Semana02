1313038b0daa9e2ea893580838432079b4493ffd
Simão Durão <simaodurao@gmail.com>
2025-02-27 11:19:04 +0000

    Added Cilindro to the sln

---
+++ Cilindro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere a Altura do Cilindro: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Insere o Raio do Cilindro: ");
+            string c = Console.ReadLine();
+        }
+    }
+}
