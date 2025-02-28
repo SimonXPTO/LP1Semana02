f2b4b8f44f117f570348829106fca0ba4d907374
Simão Durão <simaodurao@gmail.com>
2025-02-28 17:27:29 +0000

    Fixed some errors on the WriteLine

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -18,7 +18,7 @@ namespace GuessTheNumber
 
                 if (b<0 | b>30)
                 {
-                    Console.WriteLine("Numbers must be between 0 and 30. Try again");
+                    Console.WriteLine("Numbers must be between 0 and 30. Try again.");
                     counter++;
                 }
                 else if (b<numberToGuess)
@@ -28,13 +28,13 @@ namespace GuessTheNumber
                 }
                 else if (b>numberToGuess)
                 {
-                    Console.WriteLine($"The hidden number is lower than {b}. Try Again");
+                    Console.WriteLine($"The hidden number is lower than {b}. Try Again.");
                     counter++;
                 }
                 else
                 {
                     ++counter;
-                    Console.WriteLine($"You found the hidden number {numberToGuess} after {counter} tries");
+                    Console.WriteLine($"You found the hidden number {numberToGuess} after {counter} tries.");
                     break;
                 }
             }
